5682d0c28f323396ffac79b1f50cfbfff9f169eb
buivanthuong <bui.van.thuong@sun-asterisk.com>
2021-12-13 16:33:08 +0700

    update frontend

---
+++ student_frontend/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="watchers">
+    <div>{{ reverseMessage }}</div>
+      <div><button @click="changeMessage">Click me</button></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        message: 'Trứng rán cần mỡ'
+      }
+    },
+    methods: {
+        changeMessage () {
+            this.message = 'Cần cậu cơ'
+        }
+    },
+    computed: {
+      reverseMessage () {
+        return this.message.split('').reverse().join('')
+      }
+    },
+    watch: {
+      reverseMessage () {
+        console.log('Computed changed')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
